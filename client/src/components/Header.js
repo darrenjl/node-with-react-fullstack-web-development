7f2cde51855a7214d87041905f49d7437bf329de
Darren Lyons <d.lyons88@gmail.com>
2018-03-27 01:18:24 +1100

    Section 7: configuring header to display content based on data in store populated through redux by calling server endpoints using axios

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,17 +1,41 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login with google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a href="#" className="left brand-logo">
+          <Link
+            to={this.props.auth ? "/surveys" : "/"}
+            className="left brand-logo"
+          >
             Emaily
-          </a>
+          </Link>
+
           <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li>
-              <a href="/auth/google">Login with google</a>
-            </li>
+            {this.renderContent()}
+            {/*  */}
           </ul>
         </div>
       </nav>
@@ -19,4 +43,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
