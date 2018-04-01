664114c877ee9ab52fef87cc65547c859acef787
Darren Lyons <d.lyons88@gmail.com>
2018-04-01 15:59:00 +1000

    Section 10: improving email template

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -3,31 +3,39 @@ const keys = require("../config/keys");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
 const Mailer = require("../services/Mailer");
-const surveyTemplate = require("../services/emailTemplates/surveyTemplate")
+const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 const Survey = mongoose.model("surveys");
 
 module.exports = app => {
-  app.get("/api/surveys", async (req, res) => {
+  app.get("/api/surveys/thanks", (req, res) => {
+    res.send("Thanks for your feedback");
+  });
+
+  app.get("/api/surveys", requireLogin, async (req, res) => {
     const surveys = await Survey.find();
     res.send(surveys);
   });
 
-  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
+  app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recepients } = req.body;
     const survey = new Survey({
       title,
       subject,
       body,
-      recepients: recepients.split(',').map(email => ({ email: email.trim() })),
+      recepients: recepients.split(",").map(email => ({ email: email.trim() })),
       dateSent: Date.now()
     });
     const mailer = new Mailer(survey, surveyTemplate(survey));
+    try {
+      await mailer.send();
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await req.user.save();
 
-    mailer.send();
-
-
-    console.log(survey);
-    res.sendStatus(201);
+      res.send(user);
+    } catch (err) {
+      res.status(422).send(err);
+    }
   });
 
   app.post("/api/surveys/webhooks", (req, res) => {
